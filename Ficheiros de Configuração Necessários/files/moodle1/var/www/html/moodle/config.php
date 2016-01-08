4f6ada22aad964c75de818cc666290e03c993d61
João Veloso <7140201@my.ipleiria.pt>
2016-01-08 02:51:46 +0000

    Ficheiros de Configuração Necessários
    Ficheiros de Configuração Necessários - 08-01-2016 - 02:50H

---
+++ Ficheiros de Configuração Necessários/files/moodle1/var/www/html/moodle/config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = '10.101.1.25';
+$CFG->dbname    = 'moodle';
+$CFG->dbuser    = 'moodleadmin';
+$CFG->dbpass    = '1';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://www.agrsmoodle.pt';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
